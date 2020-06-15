4993037768a34e60f06b03a705a73c96f8ae8ef8
Ridho Gumelar Bagaskara <66166071+katsuragir@users.noreply.github.com>
2020-06-15 22:56:41 +0700

    Update index.js

--- build/index.js
+++ build/index.js
@@ -10,29 +10,8 @@ const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/catalog/product/productRoutes"));
 const settingRoutes_1 = __importDefault(require("./routes/setting/settingRoutes"));
 const sendEmail_1 = __importDefault(require("./routes/sendEmail"));
-/*
-const whitelist = ['https://pay.doku.com', 'http://192.168.1.160:1234'];
-const whitelistIp = ['147.139.133.123', '103.10.130.35', '103.10.129.9'];
 
-const corsOptionsDelegate = function (req, callback) {
-    var f = /f/gi;
-    var t = /:/gi
-    const ip = req.connection.remoteAddress.replace(f,'').replace(t,'');
-    console.log(ip);
-    console.log(whitelist.indexOf(req.header('Origin')));
-    console.log(whitelistIp.indexOf(ip));
-    
-    let corsOptions;
-    
-    if (whitelist.indexOf(req.header('Origin')) !== -1 || whitelistIp.indexOf(ip) !== -1) {
-        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-    } else {
-        corsOptions = { origin: false }; // disable CORS for this request
-    }
-        callback(null, corsOptions); // callback expects two parameters: error and options
-};
-*/
-var whitelist = ['http://vapehan.com', 'http://admin.vapehan.com', 'https://www.vapehan.com', 'https://admin.vapehan.com', 'https://api.vapehan.com', 'https://pay.doku.com/', 'https://vapehan.com', 'https://www.admin.vapehan.com'];
+var whitelist = ['http://localhost/katsuragir'];
 var corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
